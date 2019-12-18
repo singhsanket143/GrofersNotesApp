9741c5b5df3f542e17c701da1009bb96434580f5
riyabansal98 <riyabansal98@gmail.com>
2019-12-18 22:55:19 +0530

    Added notes with crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191218170626) do
+ActiveRecord::Schema.define(version: 20191218172205) do
+
+  create_table "notes", force: :cascade do |t|
+    t.text "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
