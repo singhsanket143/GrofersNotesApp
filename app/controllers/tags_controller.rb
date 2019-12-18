ef1df8640025189aa18a2fcc7606f7da8e200c70
riyabansal98 <riyabansal98@gmail.com>
2019-12-19 01:06:56 +0530

    added tags search api

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,15 @@
+class TagsController < ApplicationController
+
+    def get_tag_list
+        @tags = Tag.all 
+        respond_to do |format|
+            format.json do
+              render json: { data: { tags: @tags },
+                             statusCode: 200,
+                             statusMessage: 'Successfully Extracted Tags',
+                             success: true,
+                             error: {} }
+            end
+          end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  get '/get_tag_list' => "tags#get_tag_list"
   devise_scope :user do
     get 'users/sign_out' => "devise/sessions#destroy"
   end
